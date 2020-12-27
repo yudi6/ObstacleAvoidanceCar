a29a2f254884164ec366e8892bcc7e2a4dd21fdf
yudi6 <1224243288@qq.com>
2020-12-28 01:06:20 +0800

    temp

---
+++ code/main_temp.py
@@ -0,0 +1,132 @@
+import vrep
+import time
+import numpy as np
+import cv2
+from PID import PID
+from matplotlib import pyplot as plt
+# repeat until (simRemoteApi.start(19999,1300,false,true)~=-1)
+class ObstacleAvoidanceCar():
+    def __init__(self):
+        # 客户端ID
+        self.clientId = self._connection()
+        self._get_handle()
+
+        # self._motor(1)
+        # PID控制
+        self.PID_control = PID(0.15,0.0,0.02)
+        self.PID_control.SetPoint = 0
+        # self.PID_speed_control = PID(0.2,0.02)
+        # self.PID_speed_control.SetPoint = 0.75
+        self._run()
+        # while True:
+        #     self._get_image()
+        #
+
+    def _get_handle(self):
+        # 左轮的句柄
+        _, self.left_motor = vrep.simxGetObjectHandle(self.clientId, "Pioneer_p3dx_leftMotor", vrep.simx_opmode_oneshot_wait)
+        # 右轮的句柄
+        _, self.right_motor = vrep.simxGetObjectHandle(self.clientId, "Pioneer_p3dx_rightMotor", vrep.simx_opmode_oneshot_wait)
+        # 视觉传感器句柄
+        _, self.vision_sensor = vrep.simxGetObjectHandle(self.clientId, 'Vision_sensor', vrep.simx_opmode_oneshot_wait)
+        # 获取第一张图片 为空
+        _, _, _ = vrep.simxGetVisionSensorImage(self.clientId, self.vision_sensor, 0, vrep.simx_opmode_streaming)
+        # 间隔一段时间
+        time.sleep(0.1)
+
+    def _connection(self):
+        # 关闭原有连接
+        vrep.simxFinish(-1)
+        while True:
+            # 连接
+            clientId = vrep.simxStart("127.0.0.1", 19997, True, True, 5000, 5)  # 建立和服务器的连接
+            if clientId != -1:  # 连接成功
+                print('connect successfully')
+                break
+        vrep.simxSynchronous(clientId,True)
+        #
+        return clientId
+
+    def _get_image(self):
+        _, resolution, image = vrep.simxGetVisionSensorImage(self.clientId, self.vision_sensor, 0, vrep.simx_opmode_buffer)
+        while len(image)==0:
+            _, resolution, image = vrep.simxGetVisionSensorImage(self.clientId, self.vision_sensor, 0, vrep.simx_opmode_buffer)
+        sensor_image = np.array(image, dtype=np.uint8)
+        # print(resolution)
+        # RGB图片 resolution为图片大小
+        sensor_image.resize([resolution[1], resolution[0], 3])
+        # 上下颠倒
+        sensor_image = cv2.flip(sensor_image, 0)
+        cv2.imshow('sensor_image', sensor_image)
+        return sensor_image
+
+    # 设置两个轮子的速度
+    def _motor(self, speed):
+        _ = vrep.simxSetJointTargetVelocity(self.clientId, self.left_motor, speed, vrep.simx_opmode_oneshot)
+        _ = vrep.simxSetJointTargetVelocity(self.clientId, self.right_motor, speed, vrep.simx_opmode_oneshot)
+
+    # 设置两个轮子的速度与速度差 实现转向
+    def _steer(self, speed, turn):
+        _ = vrep.simxSetJointTargetVelocity(self.clientId, self.left_motor, speed - turn, vrep.simx_opmode_oneshot)
+        _ = vrep.simxSetJointTargetVelocity(self.clientId, self.right_motor, speed + turn, vrep.simx_opmode_oneshot)
+
+    def _run(self):
+        speed_now = 10
+        error_before = 0
+        vrep.simxStartSimulation(self.clientId,vrep.simx_opmode_oneshot)
+        while True:
+            vrep.simxSynchronousTrigger(self.clientId)
+            img = self._get_image()
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            _, binary = cv2.threshold(gray, 30, 255, cv2.THRESH_BINARY)
+            binary_copy = binary.copy()
+            # print(binary_copy.shape)
+            binary_copy[binary_copy == 0] = 1
+            binary_copy[binary_copy == 255] = 0
+            binary_temp = binary_copy[-60:, 160:480].copy()
+            # binary_speed = binary_copy[-(200+int(speed_now*50)):, 300:340].copy()
+            # speed_p = np.sum(binary_speed)/(binary_speed.shape[0]*binary_speed.shape[1])
+            # if speed_p > 0.5:
+            #     speed_now += (speed_p)*0.7
+            # if speed_p <= 0.5 and speed_p > 0.1:
+            #     if speed_now > 1.5:
+            #         speed_now = 1.5
+                # else:
+                #     speed_now = 0.86*speed_now+0.42
+            print('speed:',speed_now)
+            contours, cnt = cv2.findContours(binary_temp.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            # 寻找离中点最近的线
+            nearest = 0
+            for i in range(len(contours)):
+                M = cv2.moments(contours[i])
+                center_x = int(M["m10"] / (M["m00"]+0.00000001))
+                if abs(center_x - (480-160)/2) < abs(nearest - (480-160)/2):
+                    nearest = center_x
+            # 确定方向
+            direction = nearest - (480-160)/2
+            print("d:",direction)
+            # print('nearest:',nearest)
+            temp_p = np.sum(binary_temp[-4:,:]) / (binary_temp.shape[1]*4)
+            print('PID:',self.PID_control.output)
+            print('temp_p:',temp_p)
+            if temp_p < 0.05:
+                print('1')
+                self._steer(3,0)
+                speed_now = 3
+            elif temp_p > 0.3 or (abs(error_before - direction) > 100 and len(contours) != 1):
+                print('2')
+                self._steer(10,0)
+                speed_now = 10
+            else:
+                error_before = direction
+                # 方向PID的输入
+                self.PID_control.update(direction)
+                # print("direction:",self.PID_control.output)
+                self._steer(speed_now,0)
+                speed_now = 10
+            binary = binary[-60:, 160:480]
+            cv2.imshow('image', binary)
+            if cv2.waitKey(1) == 27:
+                break
+
+car = ObstacleAvoidanceCar()
